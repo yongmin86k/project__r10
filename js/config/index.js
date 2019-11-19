f1a3086b1f9da5f0c2e0821f2b64854af32726fd
Yongmin Kim <yongmin86k@gmail.com>
2019-11-19 04:14:00 -0800

    Add theme colours

---
+++ js/config/index.js
@@ -0,0 +1,1 @@
+export {default as THEME} from './styles';
---
+++ js/config/styles.js
@@ -0,0 +1,17 @@
+const themes = {
+  color: {
+    greyM: '#999',
+    greyL: '#e6e6e6',
+    blue: '#8797D6',
+    purple: '#9963ea',
+    red: '#cf392a',
+  },
+  padding: {
+    default: {
+      paddingRight: 16,
+      paddingLeft: 16,
+    },
+  },
+};
+
+export default themes;
