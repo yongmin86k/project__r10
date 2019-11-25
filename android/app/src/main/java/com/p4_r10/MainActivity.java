3df615ce0d8f884d2ac409093ab958220e8cc513
Yongmin Kim <yongmin86k@gmail.com>
2019-11-25 00:48:34 -0800

    Create Splash screen and Appicons for Android

--- android/app/src/main/java/com/p4_r10/MainActivity.java
+++ android/app/src/main/java/com/p4_r10/MainActivity.java
@@ -1,11 +1,20 @@
 package com.p4_r10;
 
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -18,6 +27,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
+
     return new ReactActivityDelegate(this, getMainComponentName()) {
       @Override
       protected ReactRootView createRootView() {
